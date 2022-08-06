b5b5c7a0c55369c5fd505a05240466f4fcb7434b
Hovhannes Arsenyan <hovhannes.arsenyan.prm@gmail.com>
2022-08-07 01:14:51 +0300

    Update GET_TABLE_STATS.sql

--- Metadata/GET_TABLE_STATS.sql
+++ Metadata/GET_TABLE_STATS.sql
@@ -16,12 +16,12 @@ ELSE
         DECLARE @SQL NVARCHAR(MAX) = 'SELECT [ST].[name] AS [Table_Name],' + CHAR(13) + 
                                             'COUNT([IT].[TABLE_NAME]) AS [Column_Count],' + CHAR(13) + 
                                             'SUM([PA].[rows]) AS [Record_Count]' + CHAR(13) + 
-		                                    'FROM [' + @Database_name + '].[sys].[tables] AS [ST]' + CHAR(13) + 
-			                                     'INNER JOIN [' + @Database_name + '].[INFORMATION_SCHEMA].[COLUMNS] AS [IT] ON [ST].[name] = [IT].[TABLE_NAME]' + CHAR(13) + 
-			                                     'INNER JOIN [' + @Database_name + '].[sys].[partitions] AS [PA] ON [ST].[object_id] = [PA].[object_id]' + CHAR(13) + 
-		                                    'WHERE [type] = ''U''' + CHAR(13) + 
-		                                    'GROUP BY [ST].[name]' + CHAR(13) + 
-		                                    'ORDER BY [ST].[name];';
+		                             'FROM [' + @Database_name + '].[sys].[tables] AS [ST]' + CHAR(13) + 
+			                             'INNER JOIN [' + @Database_name + '].[INFORMATION_SCHEMA].[COLUMNS] AS [IT] ON [ST].[name] = [IT].[TABLE_NAME]' + CHAR(13) + 
+			                             'INNER JOIN [' + @Database_name + '].[sys].[partitions] AS [PA] ON [ST].[object_id] = [PA].[object_id]' + CHAR(13) + 
+		                             'WHERE [type] = ''U''' + CHAR(13) + 
+		                             'GROUP BY [ST].[name]' + CHAR(13) + 
+		                             'ORDER BY [ST].[name];';
 
         EXEC sp_executesql @SQL;        
     END
