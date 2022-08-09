03a648d87b5d361b2d89d851ba1a51d5516d00a7
Hovhannes Arsenyan <hovhannes.arsenyan.prm@gmail.com>
2022-08-09 08:14:49 +0300

    Update DYNAMIC_PIVOTING_WITH_PIVOT.sql

--- DYNAMIC_PIVOTING_WITH_PIVOT.sql
+++ DYNAMIC_PIVOTING_WITH_PIVOT.sql
@@ -30,29 +30,29 @@ SET @ColumnList = LEFT(@ColumnList,LEN(@ColumnList) - 1);
 
 SET @SQL = 'SELECT [Month],' + @ColumnList + CHAR(13) + 
            'FROM (' + CHAR(13) + 
-           '    SELECT [D].[EnglishMonthName] AS [Month],' + CHAR(13) + 
-           '           [D].[CalendarYear] AS [Year],' + CHAR(13) + 
-           '           [S].[SalesAmount] AS [Sales]' + CHAR(13) + 
-           '    FROM [AdventureWorksDW2017].[dbo].[FactInternetSales] AS [S]' + CHAR(13) + 
-           '    INNER JOIN [AdventureWorksDW2017].[dbo].[DimDate] AS [D] ON [S].[OrderDateKey] = [D].[DateKey]' + CHAR(13) + 
+               'SELECT [D].[EnglishMonthName] AS [Month],' + CHAR(13) + 
+                      '[D].[CalendarYear] AS [Year],' + CHAR(13) + 
+                      '[S].[SalesAmount] AS [Sales]' + CHAR(13) + 
+               'FROM [AdventureWorksDW2017].[dbo].[FactInternetSales] AS [S]' + CHAR(13) + 
+               'INNER JOIN [AdventureWorksDW2017].[dbo].[DimDate] AS [D] ON [S].[OrderDateKey] = [D].[DateKey]' + CHAR(13) + 
            ') AS [Orders]' + CHAR(13) + 
            'PIVOT (' + CHAR(13) + 
-           '    SUM([Orders].[Sales])' + CHAR(13) + 
-           '    FOR [Orders].[Year] IN (' + @YearList + ')' + CHAR(13) + 
+               'SUM([Orders].[Sales])' + CHAR(13) + 
+               'FOR [Orders].[Year] IN (' + @YearList + ')' + CHAR(13) + 
            ') AS [Pivoted_Orders]' + CHAR(13) + 
            'ORDER BY CASE [Month]' + CHAR(13) + 
-           '              WHEN ''January'' THEN 1' + CHAR(13) + 
-           '              WHEN ''February'' THEN 2' + CHAR(13) + 
-           '              WHEN ''March'' THEN 3' + CHAR(13) + 
-           '              WHEN ''April'' THEN 4' + CHAR(13) + 
-           '              WHEN ''May'' THEN 5' + CHAR(13) + 
-           '              WHEN ''June'' THEN 6' + CHAR(13) + 
-           '              WHEN ''July'' THEN 7' + CHAR(13) + 
-           '              WHEN ''August'' THEN 8' + CHAR(13) + 
-           '              WHEN ''September'' THEN 9' + CHAR(13) + 
-           '              WHEN ''October'' THEN 10' + CHAR(13) + 
-           '              WHEN ''November'' THEN 11' + CHAR(13) + 
-           '              ELSE 12' + CHAR(13) + 
-           '         END ASC;'
+                         'WHEN ''January'' THEN 1' + CHAR(13) + 
+                         'WHEN ''February'' THEN 2' + CHAR(13) + 
+                         'WHEN ''March'' THEN 3' + CHAR(13) + 
+                         'WHEN ''April'' THEN 4' + CHAR(13) + 
+                         'WHEN ''May'' THEN 5' + CHAR(13) + 
+                         'WHEN ''June'' THEN 6' + CHAR(13) + 
+                         'WHEN ''July'' THEN 7' + CHAR(13) + 
+                         'WHEN ''August'' THEN 8' + CHAR(13) + 
+                         'WHEN ''September'' THEN 9' + CHAR(13) + 
+                         'WHEN ''October'' THEN 10' + CHAR(13) + 
+                         'WHEN ''November'' THEN 11' + CHAR(13) + 
+                         'ELSE 12' + CHAR(13) + 
+                    'END ASC;'
 
 EXEC sp_executesql @SQL;
