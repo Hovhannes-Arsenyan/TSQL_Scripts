5ff2ca0c65612d97aa342a7f73e5d24ebf5ba81c
Hovhannes Arsenyan <hovhannes.arsenyan.prm@gmail.com>
2022-08-09 09:46:08 +0300

    Update DYNAMIC_PIVOTING_WITH_PIVOT.sql

--- DYNAMIC_PIVOTING_WITH_PIVOT.sql
+++ DYNAMIC_PIVOTING_WITH_PIVOT.sql
@@ -25,9 +25,9 @@ WHILE @LoopCounter <= (SELECT COUNT(*) FROM @Years)
         SET @CurrentYear = (SELECT [Year] FROM @Years WHERE [ID] = @LoopCounter);
         SET @YearList = @YearList + QUOTENAME(CAST(@CurrentYear as NVARCHAR(20))) + ',';
         SET @ColumnList = @ColumnList + 'COALESCE(ROUND(' + QUOTENAME(CAST(@CurrentYear as NVARCHAR(20))) + ',0),0) AS ' + QUOTENAME(CAST(@CurrentYear as NVARCHAR(20))) + ',';
-		SET @ColumnListColumnTotal = @ColumnListColumnTotal + 'COALESCE(ROUND(' + QUOTENAME(CAST(@CurrentYear as NVARCHAR(20))) + ',0),0)' + '+';
-		SET @ColumnListRowTotal = @ColumnListRowTotal + 'SUM(' + QUOTENAME(CAST(@CurrentYear as NVARCHAR(20))) + ')' + ',';
-		SET @ColumnListRowColumnTotal = @ColumnListRowColumnTotal + 'SUM(' + QUOTENAME(CAST(@CurrentYear as NVARCHAR(20))) + ')' + '+';
+        SET @ColumnListColumnTotal = @ColumnListColumnTotal + 'COALESCE(ROUND(' + QUOTENAME(CAST(@CurrentYear as NVARCHAR(20))) + ',0),0)' + '+';
+        SET @ColumnListRowTotal = @ColumnListRowTotal + 'SUM(' + QUOTENAME(CAST(@CurrentYear as NVARCHAR(20))) + ')' + ',';
+        SET @ColumnListRowColumnTotal = @ColumnListRowColumnTotal + 'SUM(' + QUOTENAME(CAST(@CurrentYear as NVARCHAR(20))) + ')' + '+';
         SET @LoopCounter = @LoopCounter + 1
     END
 
@@ -41,8 +41,8 @@ SELECT @ColumnListColumnTotal
 
 SET @SQL = 'WITH CTE_Pivot_Temp ([Month],' + @YearList + ',[GRAND TOTAL])' + CHAR(13) + 
            'AS' + CHAR(13) + 
-		   '(' + CHAR(13) + 
-		       'SELECT [Month],' + @ColumnList + ',' + @ColumnListColumnTotal + 'AS [GRAND TOTAL]' + CHAR(13) + 
+           '(' + CHAR(13) + 
+               'SELECT [Month],' + @ColumnList + ',' + @ColumnListColumnTotal + 'AS [GRAND TOTAL]' + CHAR(13) + 
                'FROM (' + CHAR(13) + 
                    'SELECT [D].[EnglishMonthName] AS [Month],' + CHAR(13) + 
                           '[D].[CalendarYear] AS [Year],' + CHAR(13) + 
@@ -54,29 +54,29 @@ SET @SQL = 'WITH CTE_Pivot_Temp ([Month],' + @YearList + ',[GRAND TOTAL])' + CHA
                    'SUM([Orders].[Sales])' + CHAR(13) + 
                    'FOR [Orders].[Year] IN (' + @YearList + ')' + CHAR(13) + 
                ') AS [Pivoted_Orders]' + CHAR(13) + 
-		   ')' + CHAR(13) + 
-		   '' + CHAR(13) + 
-		   'SELECT * ' + CHAR(13) + 
-		   'FROM' + CHAR(13) + 
-		       '((SELECT [Month],' + @YearList + ',[GRAND TOTAL]' + CHAR(13) + 
-		       'FROM CTE_Pivot_Temp)' + CHAR(13) + 
-		       'UNION' + CHAR(13) + 
-		       '(SELECT ''TOTAL'',' + @ColumnListRowTotal + ',' + @ColumnListRowColumnTotal + CHAR(13) + 
-		       'FROM CTE_Pivot_Temp)) AS [SQ]' + CHAR(13) + 
-		   'ORDER BY CASE [Month]' + CHAR(13) + 
-                         'WHEN ''January'' THEN 1' + CHAR(13) + 
-                         'WHEN ''February'' THEN 2' + CHAR(13) + 
-                         'WHEN ''March'' THEN 3' + CHAR(13) + 
-                         'WHEN ''April'' THEN 4' + CHAR(13) + 
-                         'WHEN ''May'' THEN 5' + CHAR(13) + 
-                         'WHEN ''June'' THEN 6' + CHAR(13) + 
-                         'WHEN ''July'' THEN 7' + CHAR(13) + 
-                         'WHEN ''August'' THEN 8' + CHAR(13) + 
-                         'WHEN ''September'' THEN 9' + CHAR(13) + 
-                         'WHEN ''October'' THEN 10' + CHAR(13) + 
-                         'WHEN ''November'' THEN 11' + CHAR(13) + 
-                         'WHEN ''December'' THEN 12' + CHAR(13) + 
-						 'WHEN ''TOTAL'' THEN 13' + CHAR(13) + 
-                    'END ASC;'
+           ')' + CHAR(13) + 
+           '' + CHAR(13) + 
+           'SELECT * ' + CHAR(13) + 
+           'FROM' + CHAR(13) + 
+               '((SELECT [Month],' + @YearList + ',[GRAND TOTAL]' + CHAR(13) + 
+               'FROM CTE_Pivot_Temp)' + CHAR(13) + 
+               'UNION' + CHAR(13) + 
+               '(SELECT ''TOTAL'',' + @ColumnListRowTotal + ',' + @ColumnListRowColumnTotal + CHAR(13) + 
+               'FROM CTE_Pivot_Temp)) AS [SQ]' + CHAR(13) + 
+               'ORDER BY CASE [Month]' + CHAR(13) + 
+                             'WHEN ''January'' THEN 1' + CHAR(13) + 
+                             'WHEN ''February'' THEN 2' + CHAR(13) + 
+                             'WHEN ''March'' THEN 3' + CHAR(13) + 
+                             'WHEN ''April'' THEN 4' + CHAR(13) + 
+                             'WHEN ''May'' THEN 5' + CHAR(13) + 
+                             'WHEN ''June'' THEN 6' + CHAR(13) + 
+                             'WHEN ''July'' THEN 7' + CHAR(13) + 
+                             'WHEN ''August'' THEN 8' + CHAR(13) + 
+                             'WHEN ''September'' THEN 9' + CHAR(13) + 
+                             'WHEN ''October'' THEN 10' + CHAR(13) + 
+                             'WHEN ''November'' THEN 11' + CHAR(13) + 
+                             'WHEN ''December'' THEN 12' + CHAR(13) + 
+						     'WHEN ''TOTAL'' THEN 13' + CHAR(13) + 
+                        'END ASC;'
 
 EXEC sp_executesql @SQL;
